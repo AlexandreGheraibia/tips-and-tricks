be89d1e821b136beaf00c2600865b8c6b8ed947a
gheraibia <gheraibia@hotmail.com>
2018-07-15 10:25:15 +0200

    Add files via upload

---
+++ createAP.ps1
@@ -0,0 +1,19 @@
+﻿
+if($args.count -eq 0){
+    $myProject = read-host "Please enter a project name "
+}
+else{
+    $myProject=$args[0]
+}
+#create a projet command line
+mkdir $myProject
+cd $myProject #create a angular application:
+dotnet new angular
+#create packges dotnet:
+dotnet restore
+#create the default package.json
+#mv .\ClientApp\package-lock.json package.json
+npm init -y
+npm install
+#launch visual studio
+start "$myProject.csproj" 
